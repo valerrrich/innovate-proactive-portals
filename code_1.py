90cb711dac4094a3b972738e8f8838fedaf289b4
valerrrich <161199056+valerrrich@users.noreply.github.com>
2024-03-18 19:28:55 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,13 @@
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        }
+}
+
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+        }
+        for (int item : data) {
+
+    }
